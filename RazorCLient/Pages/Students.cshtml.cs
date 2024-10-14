ddafc5502404b5ea511545738df4b86322c907ba
Doron Shafrir <doron.shafrir.74@gmail.com>
2024-10-14 15:58:52 +0300

    last version

--- RazorCLient/Pages/Students.cshtml.cs
+++ RazorCLient/Pages/Students.cshtml.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using ServiceReference1;
+using ServiceReference2;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
@@ -10,25 +10,29 @@ namespace RazorCLient.Pages
     public class StudentsModel : PageModel
     {
         [BindProperty]
-        public string studentID { get; set; } 
-        public StudentList list { get; set; }
+        public string studentID { get; set; }
         public Student student { get; set; } = new Student();
+        public StudentList list { get; set; } = new StudentList();
+        [BindProperty]
+        public CityList cityList { get; set; } = new CityList();
+        public City city { get; set; } = new City();
         [BindProperty]
         public string firstName { get; set; }
         [BindProperty]
         public string lastName { get; set; }
-        public string hide { get; set; } 
+        public string hide { get; set; }
         public string notHide { get; set; }
-       
+        [BindProperty]
+        public string msg { get; set; }
         public string dispalyStatus { get; set; }
+        [BindProperty]
+        public int studentDelete {  get; set; }
 
-        
 
-        
 
         public void OnGet()
         {
-            ServiceReference1.Service1Client srv = new ServiceReference1.Service1Client();
+            ServiceReference2.Service1Client srv = new ServiceReference2.Service1Client();
             list = srv.GetStudentList();
             srv.Close();
             if (string.IsNullOrEmpty(HttpContext.Session.GetString("InsertStudent")))
@@ -40,22 +44,23 @@ namespace RazorCLient.Pages
         }
         public IActionResult OnPostSelectAll()
         {
-            ServiceReference1.Service1Client srv = new ServiceReference1.Service1Client();
+            ServiceReference2.Service1Client srv = new ServiceReference2.Service1Client();
             list = srv.GetStudentList();
             srv.Close();
             return Page();
-            
+
         }
 
         public IActionResult OnPostSearchById(string studentID)
         {
-            if (!String.IsNullOrEmpty(studentID)) { 
-            int id = int.Parse(studentID);
-            Service1Client srv = new Service1Client();
-            list = srv.GetStudentListById(id);
-            srv.Close();
-            return Page();
-                
+            if (!String.IsNullOrEmpty(studentID))
+            {
+                int id = int.Parse(studentID);
+                Service1Client srv = new Service1Client();
+                list = srv.GetStudentListById(id);
+                srv.Close();
+                return Page();
+
             }
             return Page();
         }
@@ -65,7 +70,7 @@ namespace RazorCLient.Pages
             if (!String.IsNullOrEmpty(firstName) &&
                 !String.IsNullOrEmpty(lastName))
             {
-                
+
                 Service1Client srv = new Service1Client();
                 list = srv.GetStudentListByName(firstName, lastName);
                 srv.Close();
@@ -73,39 +78,89 @@ namespace RazorCLient.Pages
 
             }
             return Page();
-        }  
+        }
 
         public IActionResult OnPostAddStudentDisplay()
         {
 
             dispalyStatus = HttpContext.Session.GetString("InsertStudent");
-            if (dispalyStatus == "True") {hide = "none"; notHide = "block"; HttpContext.Session.SetString("InsertStudent","False"); }
-            else { hide = "block"; notHide = "none"; HttpContext.Session.SetString("InsertStudent", "True"); OnPostSelectAll(); }
+            if (dispalyStatus == "True")
+            {
+                Service1Client srv = new Service1Client();
+                cityList = srv.GetCityList();
+                srv.Close();
+                hide = "none"; notHide = "block";
+                HttpContext.Session.SetString("InsertStudent", "False");
+                OnPostSelectAll();
+            }
+            else
+            {
+                hide = "block";
+                notHide = "none";
+                HttpContext.Session.SetString("InsertStudent", "True");
+                OnPostSelectAll();
+            }
 
 
             return Page();
         }
-      
-    public IActionResult OnPostInsertStudent( string FName, string LName, string CityName, String Phone)
-    {
-            student = new Student()
+
+
+
+
+
+
+
+        public IActionResult OnPostInsertStudent(string FName, string LName, String Phone, int cities)
+        {
+            int done = -1;
+            if (!String.IsNullOrEmpty(FName) && !String.IsNullOrEmpty(LName) && cities != 0)
             {
-                Fname = FName,
-                Lname = LName,             
-                Phone = int.Parse(Phone)
-            };
-            ServiceReference1.Service1Client srv = new ServiceReference1.Service1Client();
-            int done = srv.GetInsertStudent(student);
 
-            srv.Close();
+                city.Id = cities;
+                student = new Student()
+                {
+                    Fname = FName,
+                    Lname = LName,
+                    City = city,
+
+                };
+
+                if (!String.IsNullOrEmpty(Phone)) student.Phone = int.Parse(Phone);
 
 
+
+                Service1Client srv = new Service1Client();
+                done = srv.GetInsertStudent(student);
+                srv.Close();
+                if (done == 0) msg = "Student added succesfuly";
+            }
+
+            else
+            {
+                msg = "fields cannot stay empty";
+                Service1Client srv = new Service1Client();
+                cityList = srv.GetCityList();
+                srv.Close();
+            }
 
             return Page();
 
+        }
+
+        public IActionResult OnPostDeleteStudent(int studentDelete)
+        {
+            Service1Client srv = new Service1Client();
+            srv.GetCreateDeleteByIdSql(studentDelete);
+            srv.Close();
+            hide = "block";
+            notHide = "none";
+            HttpContext.Session.SetString("InsertStudent", "True");
+            OnPostSelectAll();
+            return Page();
+        }
+
+          
     }
 
-
-    }
-    
 }
